aa87b2db324f7e1d0968e9a1c85af14d239dc180
csvital <hayati.ibis@yandex.com>
2017-06-20 15:14:04 +0300

    major changes

--- cardstyle-tracker.js
+++ cardstyle-tracker.js
@@ -8,7 +8,6 @@ class Card {
 		this.cardFields = [];
 	}
 }
-
 class SubField {
 	constructor(subFieldOrder,subFieldType,subFieldId,subFieldName){
 		this.subFieldOrder = subFieldOrder;
@@ -20,24 +19,27 @@ class SubField {
 	}
 }
 
-
-
-function startTimer(){
-    console.log("focused!! on " + this.id);
-    whenFocus = new Date();
-    console.log(whenFocus);
+// text field'da gecen sureyi
+// hesaplayan adamlar
+function textTimeCalculator(flag){
+	//console.log(">>>>>>");
+	//console.log(flag.type);
+	// flag has event attributes
+	if(flag.type == 'focus'){
+    	console.log("focused!! on " + this.id);
+    	whenFocus = new Date();
+    	//console.log(whenFocus);
+	}else if (flag.type == 'blur') {
+		console.log("blured!! from " +this.id);
+    	whenBlur = new Date();
+    	//console.log(whenBlur);
+    	var tinyElapsed = whenBlur - whenFocus;
+    	console.log("User spend " + tinyElapsed/1000 + " seconds on " + this.id + " field on unique event.");
+	}
 }
 
-function endTimer(){
-    console.log("blured!! from " +this.id);
-    whenBlur = new Date();
-    console.log(whenBlur);
-    var tinyElapsed = whenBlur - whenFocus;
-    console.log("User spend " + tinyElapsed/1000 + " seconds on " + this.id + " field on unique event.");
-}
-
+// question field detailed reader and executioner
 var cardList = [];
-
 var cardOrder = 0;
 function cardReader(jfQ){ // jfQ > jotform Question object
 	var fieldOrder = 0;
@@ -45,30 +47,21 @@ function cardReader(jfQ){ // jfQ > jotform Question object
 	console.log("cardOrder : " + cardOrder++);
 	//console.log(jfQ.parentNode.parentNode.parentNode.getAttribute('data-type'));
 
-	console.log(jfQ.children);
+	//console.log(jfQ.children);
 
-	console.log('>>');
-	console.log(jfQ.getElementsByTagName('input'));
+	//console.log('>>');
+	//console.log(jfQ.getElementsByTagName('input'));
 
 	var inputs = jfQ.getElementsByTagName('input');
 
-	console.log(inputs[0]);
+	//console.log(inputs[0]);
 
-	inputs[0].addEventListener("focus", function(){
-    			console.log("focused!! on " + this.id);
-    			whenFocus = new Date();
-    			console.log(whenFocus);
-				});
-	inputs[0].addEventListener("blur", function(){
-				console.log("blured!! from " +this.id);
-    			whenBlur = new Date();
-    			console.log(whenBlur);
-    			var tinyElapsed = whenBlur - whenFocus;
-    			console.log("User spend " + tinyElapsed/1000 + " seconds on " + this.id + " field on unique event.");
-				});
-
-	//jfQ.children.childNodes[0].addEventListener("focus", startTimer, true);
-	//jfQ.children.childNodes[0].addEventListener("blur", endTimer, true);
+	// this block counts time for 
+	// text fields focus and blur events
+	for (var i = 0; i < inputs.length; i++) {
+		inputs[i].addEventListener("focus", textTimeCalculator);
+		inputs[i].addEventListener("blur", textTimeCalculator);
+	}
 
 	// li element's control type
 	var control_type = jfQ.parentNode.parentNode.parentNode.getAttribute('data-type').split("control_");
@@ -78,18 +71,74 @@ function cardReader(jfQ){ // jfQ > jotform Question object
 	for (var i = 0; i < jfQ.childElementCount; i++) {
 		console.log("\tfieldOrder : " + fieldOrder++);	
 	}
-
-
-
 }
 
+function findIsVisibleCard(){
+	console.log("welcome to my termination");
+	visibleCard = document.querySelectorAll('.isVisible');
+	cardStartTime = new Date();
+	console.log(visibleCard);
+	console.log("i'm the voice of war");
+}
+
+function cardOnChange(){
+	console.log("nexte, progresse veya preve basti");
+	console.log(this);
+	cardEndTime = new Date();
+	elapsedTime = cardEndTime - cardStartTime;
+	console.log("User spend " + elapsedTime/1000 + " seconds on " + this.id + " card on unique view.");
+	findIsVisibleCard();
+}
+
+// Initialize the widget only select question fields and 
+// pass them to reader
 function init () {
 	var jfQuestionFields = document.getElementsByClassName("jfQuestion-fields");
 	console.log(jfQuestionFields);
 	for (var i = 0; i < jfQuestionFields.length; i++) {
 		cardReader(jfQuestionFields[i]);
 	}
+	
+	// visible olani bul
+	findIsVisibleCard();
 
+	// next'e basildi mi?
+	// find next buttons and add listener for onchange events
+	var nextButtons = document.querySelectorAll('.forNext');
+	console.log(nextButtons);
+	for (var i = 0; i < nextButtons.length; i++) {
+		nextButtons[i].addEventListener("click", cardOnChange);
+	}
+	var prevButtons = document.querySelectorAll('.forPrev');
+	console.log(prevButtons);
+	for (var i = 0; i < prevButtons.length; i++) {
+		prevButtons[i].addEventListener("click", cardOnChange);
+	}
+
+	var progressItems = document.querySelectorAll('.jfProgress-item');
+	console.log("Progress Items");
+	console.log(progressItems);
+	for (var i = 0; i < progressItems.length; i++) {
+		progressItems[i].addEventListener("click", cardOnChange);
+	}
+
+	// EOC
+
+
+
+
+
+	// textArea for detailed report
+	var textArea = document.getElementsByTagName('textarea');
+	console.log(textArea);
+	textArea[0].innerHTML = "Sa";
 
 }
+
+
+
 window.onload = init;
+
+
+
+
