dc54b805b69fd9ae10faeaccc1597f064099ecac
csvital <hayati.ibis@yandex.com>
2017-06-20 12:45:46 +0300

    class add

--- cardstyle-tracker.js
+++ cardstyle-tracker.js
@@ -1,9 +1,49 @@
-function init () {
-	
-	var jfQuestionFields = document.getElementsByClassName("jfQuestionFields");
-	console.log(jfQuestionFields);
-
-
+class Card {
+	constructor(cardOrder,cardType,cardName){
+		this.cardOrder = cardOrder;
+		this.cardType = cardType;
+		this.cardName = cardName;
+		this.cardShowEvents = [];
+		this.cardFullTime = 0;
+		this.cardFields = [];
+	}
 }
 
+class SubField {
+	constructor(subFieldOrder,subFieldType,subFieldId,subFieldName){
+		this.subFieldOrder = subFieldOrder;
+		this.subFieldType = subFieldType;
+		this.subFieldId = subFieldId;
+		this.subFieldName = subFieldName;
+		this.subFieldEvents = [];
+		this.subFieldFullTime = 0;
+	}
+}
+
+var cardList = [];
+
+var cardOrder = 0;
+function cardReader(jfQ){ // jfQ > jotform Question object
+	var fieldOrder = 0;
+	
+	console.log("cardOrder : " + cardOrder++);
+	console.log(jfQ.parentNode.parentNode.parentNode.getAttribute('data-type'));
+
+	// li element's control type
+	var control_type = jfQ.parentNode.parentNode.parentNode.getAttribute('data-type').split("control_");
+	
+	console.log("\t"+control_type[1]);
+	
+	for (var i = 0; i < jfQ.childElementCount; i++) {
+		console.log("\tfieldOrder : " + fieldOrder++);	
+	}
+}
+
+function init () {
+	var jfQuestionFields = document.getElementsByClassName("jfQuestion-fields");
+	console.log(jfQuestionFields);
+	for (var i = 0; i < jfQuestionFields.length; i++) {
+		cardReader(jfQuestionFields[i]);
+	}
+}
 window.onload = init;
---
+++ dump.js
@@ -0,0 +1,3 @@
+function typeOf (obj) {
+  return {}.toString.call(obj).split(' ')[1].slice(0, -1).toLowerCase();
+}
